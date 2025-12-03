aedbff0980dafa31d07d072d3a1028ec368b2b88
moiV123 <valentin.blp@gmail.com>
2025-12-03 18:32:02 +0100

    Changement_pres

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for, session  # type: ignore
+import pymongo  # type: ignore
+import os
+import certifi
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=81, debug=True)
