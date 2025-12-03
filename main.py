d2b15f5542a14a509c5e9d29595dad3f62f52a6d
moiV123 <valentin.blp@gmail.com>
2025-12-03 19:00:38 +0100

    ajout_route_login

--- main.py
+++ main.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=81, debug=True)
