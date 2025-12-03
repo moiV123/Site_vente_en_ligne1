7c0a4af11a0535c4679f9840add0864e894ab088
moiV123 <valentin.blp@gmail.com>
2025-12-03 19:07:27 +0100

    ajout_route_register

--- main.py
+++ main.py
@@ -13,5 +13,9 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=81, debug=True)
