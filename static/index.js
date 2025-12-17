749e7f12474554da43287c926a01ceedbdc73abc
moiV123 <valentin.blp@gmail.com>
2025-12-17 18:55:27 +0100

    ajout_du_scroller

--- static/index.js
+++ static/index.js
@@ -11,9 +11,28 @@ window.addEventListener("load", () => {
   // Animate loader fade out
   gsap.to("#loader", {
     opacity: 0,
-    duration: 0.5,
+    duration: 1,
     onComplete: () => {
       document.getElementById("loader").style.display = "none";
     }
   });
+});
+
+// Register ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+// Fade + slide animation for each annonce
+gsap.utils.toArray('.scroll-animate').forEach((elem) => {
+    gsap.from(elem, {
+        scrollTrigger: {
+            trigger: elem,
+            start: "top 80%", // quand le top de l’élément atteint 80% du viewport
+            toggleActions: "play none none none"
+        },
+        opacity: 0,
+        y: 50, // glisse depuis le bas
+        duration: 0.8,
+        ease: "power2.out",
+        stagger: 0.2
+    });
 });
